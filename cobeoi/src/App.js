606271910e649d99df4bb4e94d7b32d7551ea447
Y Bui <buinhuy312@gmail.com>
2026-01-16 23:44:22 +0700

    code

--- cobeoi/src/App.js
+++ cobeoi/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
+    {/* nyne */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit minh oi<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
